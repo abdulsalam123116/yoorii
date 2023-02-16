cdd9e566acee077cad6b7c8fccc2569a2c91722d
uaesmartlink <uaesmartlink@gmail.com>
2023-02-16 19:36:21 +0400

    init

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,11 +2,11 @@ const mix = require('laravel-mix');
 const CompressionPlugin = require("compression-webpack-plugin");
 const path = require('path');
 
-let css_plugin = 'public/frontend/css/';
-let js_plugin = 'public/frontend/js/';
+let css_plugin = 'frontend/css/';
+let js_plugin = 'frontend/js/';
 mix.setResourceRoot(process.env.MIX_ASSET_URL);
 
-mix.js('resources/js/app.js', 'public/frontend/js')
+mix.js('resources/js/app.js', 'frontend/js')
     .vue().combine([
     css_plugin + 'bootstrap.min.css',
     css_plugin + 'animate.min.css',
@@ -15,14 +15,14 @@ mix.js('resources/js/app.js', 'public/frontend/js')
     css_plugin + 'development.css',
     css_plugin + 'responsive.css',
     css_plugin + 'vue-plyr.css',
-], 'public/frontend/css/app.css').combine([
+], 'frontend/css/app.css').combine([
     js_plugin + 'html5shiv.min.js',
     js_plugin + 'respond.min.js',
-], 'public/frontend/js/plugin.js').webpackConfig({
+], 'frontend/js/plugin.js').webpackConfig({
     output: {
-        chunkFilename: "public/frontend/js/chunks-160/[name].[chunkhash].js",
+        chunkFilename: "frontend/js/chunks-160/[name].[chunkhash].js",
         publicPath: 'auto',
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, ''),
     },
     plugins: [
         // new BundleAnalyzerPlugin(),      // load this package to see which plugins with its size detail
@@ -36,7 +36,7 @@ mix.js('resources/js/app.js', 'public/frontend/js')
     ]
 });
 
-mix.js('resources/js/admin.js', 'public/admin/js/app.js').vue();
+mix.js('resources/js/admin.js', 'admin/js/app.js').vue();
 
 
 mix.version();
