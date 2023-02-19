f136667d8b512a8e263657121ca6619c5a35dd95
abdulsalam alkhateeb <abdulsalam@matched.se>
2023-02-19 19:08:14 +0200

    commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,7 +20,7 @@ mix.js('resources/js/app.js', 'public/frontend/js')
     js_plugin + 'respond.min.js',
 ], 'public/frontend/js/plugin.js').webpackConfig({
     output: {
-        chunkFilename: "public/frontend/js/chunks-160/[name].[chunkhash].js",
+        chunkFilename: "frontend/js/chunks-160/[name].[chunkhash].js",
         publicPath: 'auto',
         path: path.resolve(__dirname, 'public'),
     },
